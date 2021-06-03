8149c0ef68ad902112297d365c5cb1401b89a219
anton851 <trialtech@rambler.ru>
2021-06-03 23:29:24 +0300

    решил задачу Проверка на спам

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
-function checkSpam(str) {
-  // ваш код...
+function checkSpam (str){
+  let words = ['1XBet','XXX']
+  for(let i = 0; i < words.length; i++){
+      if(!str) return str;
+      if (str.toLowerCase().includes(words[i].toLowerCase())) return true;
+  }
+  return false;
 }
+
+checkSpam('1XbeT');
+checkSpam('free xxxxx');
+checkSpam("innocent rabbit");
