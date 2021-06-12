e395cb30fad70bf188a7cdb5ee04518fc730a4c1
anton851 <trialtech@rambler.ru>
2021-06-12 16:32:07 +0300

    решил задачу Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,19 @@
 function getMinMax(str) {
-  // ваш код...
+  whoa = [...str]
+  .map(i => {if(isFinite(i) == true || i == "." || i == "-"){
+    return i
+  } else {
+    return " "}
+  })
+  .join("")
+  .split(" ")
+  .filter(i => i != "")
+  .map(i => Number(i));
+  let min = Math.min(...whoa);
+  let max = Math.max(...whoa);
+  let finalObj = {
+    min: min ,
+    max: max
+  };
+  return finalObj;
 }
